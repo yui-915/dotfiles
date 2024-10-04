7a848e2ea332e5fda1772eed9c0f19ced30145f0
Yui <yui300127@gmail.com>
2024-10-05 02:00:15 +0300

    starship

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,10 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+
+  # prompt (starship)
+  function starship_transient_prompt_func
+    starship module character
+  end
+  starship init fish | source
+  enable_transience
+
 end
